c8b1456511f5622c0430eaaa554e967abab087a3
dnz <dennizwarui@gmail.com>
2021-05-02 08:49:44 +0300

    implement contact_us form

--- js/index.js
+++ js/index.js
@@ -16,6 +16,49 @@ function animate_skills() {
     i_skill += 1;
 }
 
+function showFormMessage(message, color){
+    let msg = $("<div class=\"message\"></div>");
+    msg.html(message)
+    msg.css({'background-color': color})
+    $('#contact_us').append(msg)
+
+    msg.animate({
+        height: '20px'
+    }, 100, function () {
+        setTimeout(() => {
+            msg.remove()
+        }, 5000)
+    })
+}
+
+function hijackForm(form) {
+    let name = $('#form_name')
+        , email = $('#form_email')
+        , message = $('#form_message');
+
+    let data = {
+        'name': name.val(),
+        'email': email.val(),
+        'message': message.val()
+    }
+
+    $.ajax({
+        url: '/api/v1/contact_us',
+        method: 'POST',
+        data: data,
+        dataType: "*",
+        success: function () {
+            name.val('')
+            email.val('')
+            message.val('')
+            showFormMessage("Success", 'green')
+        },
+        error: function (e) {
+            showFormMessage("Failed", 'red')
+        }
+    })
+}
+
 $(document).ready(function () {
     $('#menu_links a').click(function () {
         if ($(window).width() < 768) {
@@ -33,4 +76,11 @@ $(document).ready(function () {
     skills_description = $('#skill_description')
     skills = $('#skills_icons img');
     setInterval(animate_skills, 3000);
+
+    $('#contact_us_form').on('submit', function (e) {
+        e.preventDefault()
+        e.returnValue = false
+
+        hijackForm($(this))
+    })
 })
